fc3e914d35c1dc7ff6de90f93b7373ae458203f8
Adisin2209 <adisin2209@gmail.com>
2024-12-05 18:10:10 +0100

    1.1

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Nach der gewünschten Distribution fragen
 echo ""
-echo "==UberEats MinMaxxer simple Installer=="
-echo "!!--UberEats MinMaxxer requires --Maven, Chromedriver, Chrome-- --!!"
+echo "== UberEats MinMaxxer Simple Installer =="
+echo "!!-- UberEats MinMaxxer requires --Maven, Chromedriver, Chrome, OpenJDK-- !!"
 echo ""
 echo "Which Linux-Distro are you using?"
 echo "0 - Ubuntu"
@@ -13,15 +13,30 @@ read -r distro
 
 # Abhängig von der Eingabe die entsprechende Aktion ausführen
 if [ "$distro" -eq 1 ]; then
-    echo "Installing dependancies"
-    sudo pacman -S maven
-    yay -S chromedriver google-chrome
+    echo "Installing dependencies for Arch Linux..."
     
-elif [ "$distro" -eq 0 ]; then
-    echo "Installing dependancies"
+    # OpenJDK installieren
+    echo "Installing OpenJDK..."
+    sudo pacman -S --needed jdk-openjdk
     
     # Maven installieren
+    sudo pacman -S --needed maven
+    
+    # Chrome und Chromedriver installieren
+    echo "Installing Google Chrome and Chromedriver..."
+    yay -S --needed chromedriver google-chrome
+
+elif [ "$distro" -eq 0 ]; then
+    echo "Installing dependencies for Ubuntu..."
+    
+    # System-Updates
     sudo apt update
+    
+    # OpenJDK installieren
+    echo "Installing OpenJDK..."
+    sudo apt install -y openjdk-21-jdk
+    
+    # Maven installieren
     sudo apt install -y maven wget unzip
     
     # Google Chrome installieren
@@ -39,13 +54,23 @@ elif [ "$distro" -eq 0 ]; then
     sudo mv chromedriver /usr/local/bin/
     chmod +x /usr/local/bin/chromedriver
     rm chromedriver_linux64.zip
-    
+
 else
     echo "Ungültige Eingabe. Bitte 1 oder 0 eingeben."
+    exit 1
+fi
+
+# Überprüfen, ob Java korrekt installiert wurde
+echo "Checking Java installation..."
+java -version
+if [ $? -ne 0 ]; then
+    echo "Java installation failed. Please check your system and try again."
+    exit 1
 fi
 
 # mvn package clean ausführen
-echo "Running 'mvn package clean'..."
+echo "Running 'mvn clean package'..."
 mvn clean package
 
 echo "Installation complete."
+
